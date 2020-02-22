c377e80a6edfa0b1457ae3a79a02c7ad8f105487
MEYTA <43209941+serdaraltin@users.noreply.github.com>
2020-02-22 07:18:59 +0300

    Add files via upload

---
+++ Hdd Serial Number/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CarbonFiberTheme1 = New Hdd_Serial_Number.CarbonFiberTheme()
+        Me.CarbonFiberButton2 = New Hdd_Serial_Number.CarbonFiberButton()
+        Me.CarbonFiberLabel2 = New Hdd_Serial_Number.CarbonFiberLabel()
+        Me.CarbonFiberProgressBar1 = New Hdd_Serial_Number.CarbonFiberProgressBar()
+        Me.CarbonFiberTextBox1 = New Hdd_Serial_Number.CarbonFiberTextBox()
+        Me.CarbonFiberLabel1 = New Hdd_Serial_Number.CarbonFiberLabel()
+        Me.CarbonFiberButton1 = New Hdd_Serial_Number.CarbonFiberButton()
+        Me.CarbonFiberTheme1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 20
+        '
+        'CarbonFiberTheme1
+        '
+        Me.CarbonFiberTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(22, Byte), Integer), CType(CType(22, Byte), Integer))
+        Me.CarbonFiberTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.CarbonFiberTheme1.Colors = New Hdd_Serial_Number.Bloom(-1) {}
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton2)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberLabel2)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberProgressBar1)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberTextBox1)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberLabel1)
+        Me.CarbonFiberTheme1.Controls.Add(Me.CarbonFiberButton1)
+        Me.CarbonFiberTheme1.Customization = ""
+        Me.CarbonFiberTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CarbonFiberTheme1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberTheme1.Icon = Nothing
+        Me.CarbonFiberTheme1.Image = Nothing
+        Me.CarbonFiberTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.CarbonFiberTheme1.Movable = True
+        Me.CarbonFiberTheme1.Name = "CarbonFiberTheme1"
+        Me.CarbonFiberTheme1.NoRounding = False
+        Me.CarbonFiberTheme1.ShowIcon = False
+        Me.CarbonFiberTheme1.Sizable = True
+        Me.CarbonFiberTheme1.Size = New System.Drawing.Size(385, 145)
+        Me.CarbonFiberTheme1.SmartBounds = True
+        Me.CarbonFiberTheme1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.CarbonFiberTheme1.TabIndex = 0
+        Me.CarbonFiberTheme1.Text = "MAC Adres Okuyucu"
+        Me.CarbonFiberTheme1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.CarbonFiberTheme1.Transparent = False
+        '
+        'CarbonFiberButton2
+        '
+        Me.CarbonFiberButton2.Colors = New Hdd_Serial_Number.Bloom(-1) {}
+        Me.CarbonFiberButton2.Customization = ""
+        Me.CarbonFiberButton2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton2.Image = Nothing
+        Me.CarbonFiberButton2.Location = New System.Drawing.Point(331, 4)
+        Me.CarbonFiberButton2.Name = "CarbonFiberButton2"
+        Me.CarbonFiberButton2.NoRounding = False
+        Me.CarbonFiberButton2.Size = New System.Drawing.Size(44, 26)
+        Me.CarbonFiberButton2.TabIndex = 5
+        Me.CarbonFiberButton2.Text = "X"
+        Me.CarbonFiberButton2.Transparent = False
+        '
+        'CarbonFiberLabel2
+        '
+        Me.CarbonFiberLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.CarbonFiberLabel2.Colors = New Hdd_Serial_Number.Bloom(-1) {}
+        Me.CarbonFiberLabel2.Customization = ""
+        Me.CarbonFiberLabel2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberLabel2.Image = Nothing
+        Me.CarbonFiberLabel2.Location = New System.Drawing.Point(220, 113)
+        Me.CarbonFiberLabel2.Name = "CarbonFiberLabel2"
+        Me.CarbonFiberLabel2.NoRounding = False
+        Me.CarbonFiberLabel2.Size = New System.Drawing.Size(141, 14)
+        Me.CarbonFiberLabel2.TabIndex = 4
+        Me.CarbonFiberLabel2.Text = "Powered By DeadSound"
+        Me.CarbonFiberLabel2.Transparent = True
+        '
+        'CarbonFiberProgressBar1
+        '
+        Me.CarbonFiberProgressBar1.Current = 0.0R
+        Me.CarbonFiberProgressBar1.Location = New System.Drawing.Point(15, 82)
+        Me.CarbonFiberProgressBar1.Maximum = 0.0R
+        Me.CarbonFiberProgressBar1.Name = "CarbonFiberProgressBar1"
+        Me.CarbonFiberProgressBar1.Progress = 0.0R
+        Me.CarbonFiberProgressBar1.ShowPercentage = True
+        Me.CarbonFiberProgressBar1.Size = New System.Drawing.Size(346, 27)
+        Me.CarbonFiberProgressBar1.TabIndex = 3
+        Me.CarbonFiberProgressBar1.Text = "CarbonFiberProgressBar1"
+        '
+        'CarbonFiberTextBox1
+        '
+        Me.CarbonFiberTextBox1.Colors = New Hdd_Serial_Number.Bloom(-1) {}
+        Me.CarbonFiberTextBox1.Customization = ""
+        Me.CarbonFiberTextBox1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberTextBox1.Image = Nothing
+        Me.CarbonFiberTextBox1.Location = New System.Drawing.Point(54, 48)
+        Me.CarbonFiberTextBox1.MaxLength = 32767
+        Me.CarbonFiberTextBox1.Multiline = False
+        Me.CarbonFiberTextBox1.Name = "CarbonFiberTextBox1"
+        Me.CarbonFiberTextBox1.NoRounding = False
+        Me.CarbonFiberTextBox1.ReadOnly = True
+        Me.CarbonFiberTextBox1.Size = New System.Drawing.Size(223, 24)
+        Me.CarbonFiberTextBox1.TabIndex = 2
+        Me.CarbonFiberTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.CarbonFiberTextBox1.Transparent = False
+        Me.CarbonFiberTextBox1.UseSystemPasswordChar = False
+        '
+        'CarbonFiberLabel1
+        '
+        Me.CarbonFiberLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.CarbonFiberLabel1.Colors = New Hdd_Serial_Number.Bloom(-1) {}
+        Me.CarbonFiberLabel1.Customization = ""
+        Me.CarbonFiberLabel1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberLabel1.Image = Nothing
+        Me.CarbonFiberLabel1.Location = New System.Drawing.Point(15, 52)
+        Me.CarbonFiberLabel1.Name = "CarbonFiberLabel1"
+        Me.CarbonFiberLabel1.NoRounding = False
+        Me.CarbonFiberLabel1.Size = New System.Drawing.Size(33, 14)
+        Me.CarbonFiberLabel1.TabIndex = 1
+        Me.CarbonFiberLabel1.Text = "MAC"
+        Me.CarbonFiberLabel1.Transparent = True
+        '
+        'CarbonFiberButton1
+        '
+        Me.CarbonFiberButton1.Colors = New Hdd_Serial_Number.Bloom(-1) {}
+        Me.CarbonFiberButton1.Customization = ""
+        Me.CarbonFiberButton1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.CarbonFiberButton1.Image = Nothing
+        Me.CarbonFiberButton1.Location = New System.Drawing.Point(283, 45)
+        Me.CarbonFiberButton1.Name = "CarbonFiberButton1"
+        Me.CarbonFiberButton1.NoRounding = False
+        Me.CarbonFiberButton1.Size = New System.Drawing.Size(78, 29)
+        Me.CarbonFiberButton1.TabIndex = 0
+        Me.CarbonFiberButton1.Text = "Oku"
+        Me.CarbonFiberButton1.Transparent = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 145)
+        Me.Controls.Add(Me.CarbonFiberTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.CarbonFiberTheme1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents CarbonFiberTheme1 As Hdd_Serial_Number.CarbonFiberTheme
+    Friend WithEvents CarbonFiberLabel2 As Hdd_Serial_Number.CarbonFiberLabel
+    Friend WithEvents CarbonFiberProgressBar1 As Hdd_Serial_Number.CarbonFiberProgressBar
+    Friend WithEvents CarbonFiberTextBox1 As Hdd_Serial_Number.CarbonFiberTextBox
+    Friend WithEvents CarbonFiberLabel1 As Hdd_Serial_Number.CarbonFiberLabel
+    Friend WithEvents CarbonFiberButton1 As Hdd_Serial_Number.CarbonFiberButton
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents CarbonFiberButton2 As Hdd_Serial_Number.CarbonFiberButton
+
+End Class
---
+++ Hdd Serial Number/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.Win32
+Imports System.Net.NetworkInformation
+Public Class Form1
+    Dim say As Integer = 0
+    Private Sub CarbonFiberButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CarbonFiberButton1.Click
+        CarbonFiberTextBox1.Text = ""
+        say = 0
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If (say < 100) Then
+            say += 1
+            CarbonFiberProgressBar1.Progress = say
+        Else
+            Timer1.Enabled = False
+            Dim rkey As RegistryKey = Registry.LocalMachine
+            CarbonFiberTextBox1.Text = MacAddress()
+            Clipboard.SetText(CarbonFiberTextBox1.Text)
+        End If
+    End Sub
+    Function MacAddress()
+        Dim nics() As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces()
+        Return nics(0).GetPhysicalAddress.ToString()
+    End Function
+    Private Sub CarbonFiberButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CarbonFiberButton2.Click
+        Application.Exit()
+    End Sub
+End Class
